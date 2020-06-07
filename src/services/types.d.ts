63a56d61ac0e5647c7500ce4ee64c7cb2f4b01e0
André <andre@dev-next.com>
2020-06-07 00:04:37 -0300

    :sparkles: Created api functions

---
+++ src/services/types.d.ts
@@ -0,0 +1,43 @@
+type Item = {
+  id: number;
+  image: string;
+  title: string;
+};
+
+type Point = {
+  id?: number;
+  image: string;
+  name: string;
+  email: string;
+  whatsapp: string;
+  latitude: string;
+  longitude: string;
+  city: string;
+  uf: string;
+  items?: number[];
+};
+
+type IBGEUFResponse = {
+  id: number,
+  sigla: string,
+  nome: string,
+  regiao: {
+    id: number,
+    sigla: string,
+    nome: string
+  }
+};
+
+type IBGECityResponse = {
+  id: number,
+  nome: string,
+  microrregiao: {
+    id: number,
+    nome: string,
+    mesorregiao: {
+      id: number,
+      nome: string,
+      UF: IBGEUFResponse
+    }
+  }
+};
